9e50baf37d5da6083b0b71497b58e66d37396493
Daniil Voronov <d.a.voronov550@gmail.com>
2023-08-30 14:25:01 +0300

    CSRF fixes v.10000

--- .history/backend/recipebook/settings_20230830142447.py
+++ .history/backend/recipebook/settings_20230830142447.py
@@ -16,7 +16,7 @@ ALLOWED_HOSTS = ['yandex.foodgramhub.ddns.net',
                  '127.0.0.1',
                  'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['http://yandex.foodgramhub.ddns.net']
+CSRF_TRUSTED_ORIGINS = ['yandex.foodgramhub.ddns.net']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/api/.*$'
 
